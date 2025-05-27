34b26f3ec38cab00c526f9bf8bc526ffc9163087
Hadi <969hadiahmed@gmail.com>
2025-05-27 16:14:49 +0530

    feat: nested create

--- script.ts
+++ script.ts
@@ -2,7 +2,25 @@ import { PrismaClient } from "./generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.deleteMany()
+    await prisma.user.deleteMany();
+    const user = await prisma.user.create({
+        data: {
+            name: "Hadi",
+            age: 21,
+            email: "969hadi@gmail.com",
+            UserPrefrence: {
+                create: {
+                    emailUpdates: true
+                }
+            }
+        },
+        include: {
+            UserPrefrence: true
+        }
+    });
+
+    console.log(user);
+
 }
 
 main()
